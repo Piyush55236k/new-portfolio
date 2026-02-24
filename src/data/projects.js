d11a0c5561a38aba14862bfc6aa3e0e906cd01c2
Piyush Pandey <131640134+Piyush55236k@users.noreply.github.com>
2026-02-24 13:11:32 +0530

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,17 +8,9 @@ export const projects = [
     tech: ["React", "Node.js", "MongoDB", "Express"],
     description: "A MERN-based freelance marketplace with real-time bidding and authentication."
   },
+ 
   {
     id: 2,
-    title: "Weather Dashboard",
-    image: "projects/Weather.webp",
-    demo: "https://countryweatherchecker-ten.vercel.app",
-    github: "https://github.com/Piyush55236k/weather-detect",
-    tech: ["React", "API Integration"],
-    description: "Responsive weather application using OpenWeatherMap API."
-  },
-  {
-    id: 3,
     title: "Hirenova-A Hiring Platform",
     image: "projects/HireNova.webp",
     demo: "https://hirenova.vercel.app",
@@ -27,7 +19,7 @@ export const projects = [
     description: "Animated personal portfolio with scroll-based transitions."
   },
   {
-    id: 4,
+    id: 3,
     title: "Agro Mitra - SIH Project",
     image: "projects/Agro Mitra.webp",
     demo: "https://newsih-gtmo.vercel.app",
@@ -36,7 +28,7 @@ export const projects = [
     description: "Responsive e-commerce interface design."
   },
   {
-    id: 5,
+    id: 4,
     title: "Landing Page Design",
     image: "projects/Sundown.webp",
     demo: "https://sundown-web-page.vercel.app",
@@ -45,12 +37,22 @@ export const projects = [
     description: "Modern responsive landing page."
   },
   {
-    id: 6,
+    id: 5,
     title: "TextUtils-Play with text",
     image: "projects/TextUtils.webp",
     demo: "https://text-utils-blue-delta.vercel.app",
     github: "https://github.com/Piyush55236k/TextUtils",
     tech: ["React"],
     description: "Interactive UI experiment."
+  },
+
+ {
+    id: 6,
+    title: "Weather Dashboard",
+    image: "projects/Weather.webp",
+    demo: "https://countryweatherchecker-ten.vercel.app",
+    github: "https://github.com/Piyush55236k/weather-detect",
+    tech: ["React", "API Integration"],
+    description: "Responsive weather application using OpenWeatherMap API."
   }
 ];
