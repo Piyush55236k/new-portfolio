5536386a0e9469cd0b73378263c34f8fcc956f0a
Piyush Pandey <131640134+Piyush55236k@users.noreply.github.com>
2026-03-03 23:42:15 +0530

    Refactor skillsData structure for better organization

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,39 +1,39 @@
 export const skillsData = {
-  usingNow: [
+  frontend: [
     {
       id: 1,
-      src: "skills/icons8-react-48.png",
-      text: "React"
-    },
-    {
-      id: 2,
-      src: "skills/framer motion.png",
-      text: "Framer Motion"
-    },
-    {
-      id: 3,
       src: "skills/icons8-html5.svg",
       text: "HTML5"
     },
     {
-      id: 4,
+      id: 2,
       src: "skills/icons8-css3-50.png",
       text: "CSS3"
     },
     {
-      id: 5,
+      id: 3,
       src: "skills/icons8-tailwindcss.svg",
       text: "Tailwind CSS"
     },
     {
+      id: 4,
+      src: "skills/icons8-javascript.svg",
+      text: "JavaScript"
+    },
+    {
+      id: 5,
+      src: "skills/icons8-typescript.svg",
+      text: "TypeScript"
+    },
+    {
       id: 6,
-      src: "skills/icons8-mongodb.svg",
-      text: "MongoDB"
+      src: "skills/icons8-react-48.png",
+      text: "React"
     },
     {
       id: 7,
-      src: "skills/icons8-javascript.svg",
-      text: "JavaScript"
+      src: "skills/framer motion.png",
+      text: "Framer Motion"
     },
     {
       id: 8,
@@ -47,45 +47,51 @@ export const skillsData = {
     }
   ],
 
-  learning: [
+  backend: [
     {
       id: 10,
-      src: "skills/icons8-mysql.svg",
-      text: "My SQL"
-    },
-    {
-      id: 11,
       src: "skills/icons8-nodejs.svg",
       text: "Node.js"
     },
     {
-      id: 12,
-      src: "skills/icons8-typescript.svg",
-      text: "TypeScript"
-    }
-  ],
-
-  other: [
-    {
-      id: 13,
-      src: "skills/icons8-c (1).svg",
-      text: "C"
-    },
-    {
-      id: 14,
-      src: "skills/icons8-c.svg",
-      text: "C++"
-    },
-    {
-      id: 15,
+      id: 11,
       src: "skills/icons8-php.svg",
       text: "PHP"
     },
     {
-      id: 16,
+      id: 12,
       src: "skills/icons8-java.svg",
       text: "Java"
+    }
+  ],
+
+  database: [
+    {
+      id: 13,
+      src: "skills/icons8-mongodb.svg",
+      text: "MongoDB"
     },
+    {
+      id: 14,
+      src: "skills/icons8-mysql.svg",
+      text: "MySQL"
+    }
+  ],
+
+  programmingLanguages: [
+    {
+      id: 15,
+      src: "skills/icons8-c (1).svg",
+      text: "C"
+    },
+    {
+      id: 16,
+      src: "skills/icons8-c.svg",
+      text: "C++"
+    }
+  ],
+
+  tools: [
     {
       id: 17,
       src: "skills/icons8-git.svg",
